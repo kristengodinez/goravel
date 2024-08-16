87d59fb7102b77072da498b60c2a668c26d05985
kristengodinez <kristen.go@outlook.com>
2024-08-16 09:13:42 -0700

    remove user controller route

--- routes/api.go
+++ routes/api.go
@@ -7,9 +7,6 @@ import (
 )
 
 func Api() {
-	userController := controllers.NewUserController()
-	facades.Route().Get("/users/{id}", userController.Show)
-
 	luhnController := controllers.NewLuhnController()
 	facades.Route().Post("/credit_card_validation/json", luhnController.Json)
 }
