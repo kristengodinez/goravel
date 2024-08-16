b0384aef2ec92a00266741471f3adcbca0e87334
kristengodinez <kristen.go@outlook.com>
2024-08-16 08:45:01 -0700

    add unit test for json payload

--- app/http/controllers/luhn_controller.go
+++ app/http/controllers/luhn_controller.go
@@ -12,8 +12,6 @@ func NewLuhnController() *LuhnController {
 	return &LuhnController{}
 }
 
-func (r *LuhnController) Show(ctx http.Context) http.Response {
-	return ctx.Response().Success().Json(http.Json{
-		"Hello": "Goravel",
-	})
+func (r *LuhnController) Json(ctx http.Context) http.Response {
+	return ctx.Response().Success().Json(http.Json{})
 }
--- app/http/controllers/luhn_controller_test.go
+++ app/http/controllers/luhn_controller_test.go
@@ -3,6 +3,9 @@ package controllers
 import (
 	"testing"
 
+	"github.com/goravel/framework/contracts/http"
+	testingmock "github.com/goravel/framework/testing/mock"
+	"github.com/goravel/gin"
 	"github.com/stretchr/testify/suite"
 )
 
@@ -19,4 +22,33 @@ func (s *LuhnControllerTestSuite) SetupTest() {
 }
 
 func (s *LuhnControllerTestSuite) TearDownTest() {
+}
+
+func (s *LuhnControllerTestSuite) TestJson() {
+	mockFactory := testingmock.Factory()
+	mockContext := mockFactory.Context()
+	mockRequest := mockFactory.ContextRequest()
+	mockResponse := mockFactory.ContextResponse()
+	mockValidator := mockFactory.ValidationValidator()
+	mockContext.EXPECT().Request().Return(mockRequest).Once()
+	mockRequest.EXPECT().Validate(map[string]string{
+		"creditCardNumber": "required",
+	}).Return(mockValidator, nil).Once()
+	mockValidator.EXPECT().Fails().Return(false).Once()
+
+	var creditCard CreditCard
+	mockValidator.EXPECT().Bind(&creditCard).Run(func(creditCard any) {
+		creditCard.(*CreditCard).CreditCardNumber = "123"
+	}).Return(nil).Once()
+
+	mockContext.EXPECT().Response().Return(mockResponse).Once()
+	mockResponseStatus := mockFactory.ResponseStatus()
+	mockResponse.EXPECT().Success().Return(mockResponseStatus).Once()
+
+	resp := &gin.JsonResponse{}
+	mockResponseStatus.EXPECT().Json(http.Json{
+		"creditCardNumber": "123",
+	}).Return(resp).Once()
+
+	s.Equal(resp, NewLuhnController().Json(mockContext))
 }
